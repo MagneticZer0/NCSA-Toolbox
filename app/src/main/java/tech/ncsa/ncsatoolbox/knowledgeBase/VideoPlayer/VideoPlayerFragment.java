314026c73a541b0c9012527a3898d05d0b4c9d5b
MagneticZero <19812604+MagneticZer0@users.noreply.github.com>
2018-12-01 13:40:18 -0500

    Updated VideoPlayerFragment
    Added a little implemntation of the video player. It's not the best and it's basically is what I had before, but it works for all videos. This will be worked on.

--- app/src/main/java/tech/ncsa/ncsatoolbox/knowledgeBase/VideoPlayer/VideoPlayerFragment.java
+++ app/src/main/java/tech/ncsa/ncsatoolbox/knowledgeBase/VideoPlayer/VideoPlayerFragment.java
@@ -1,13 +1,16 @@
 package tech.ncsa.ncsatoolbox.knowledgeBase.VideoPlayer;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import tech.ncsa.ncsatoolbox.R;
 
@@ -18,7 +21,7 @@ public class VideoPlayerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_knowledge_base, container, false);
+        view = inflater.inflate(R.layout.fragment_knowledge_base, container, false); // Might as well reuse it, since it's empty
         return view;
     }
 
@@ -26,6 +29,23 @@ public class VideoPlayerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         String videoURL = (String) getArguments().get("video");
-        Log.i("Video link", videoURL);
+
+        VideoView videoView = new VideoView(getContext());
+        videoView.setVideoURI(Uri.parse(videoURL));
+        MediaController mediaController = new MediaController(getContext());
+        mediaController.setAnchorView(videoView);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setLayoutParams(new LinearLayout.LayoutParams(400, 400));
+        ((LinearLayout) findViewById(R.id.knowledgebaseLayout)).addView(videoView);
+    }
+
+    /**
+     * Used to return a view from th activity
+     * @param ID The ID of the view to find
+     * @return THe view with that ID
+     */
+    private View findViewById(int ID) {
+        return getView().findViewById(ID);
     }
 }
