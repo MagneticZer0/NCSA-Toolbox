7fb530d2f944ad27d71c61b06a480c239512507d
MagneticZero <19812604+MagneticZer0@users.noreply.github.com>
2018-12-13 23:43:07 -0500

    Made Unit Tests Random
    Just working on making the unit tests random just for fun and I think it's a good idea.

--- app/src/test/java/tech/ncsa/ncsatoolbox/toolbox/subnetter/SubnetterTest.java
+++ app/src/test/java/tech/ncsa/ncsatoolbox/toolbox/subnetter/SubnetterTest.java
@@ -4,11 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
 public class SubnetterTest {
     SubnetterFragment subnetterFragment;
+    int iterationsPerTest = 5;
 
     @Before
     public void setup() {
@@ -19,22 +21,55 @@ public class SubnetterTest {
     public void fix() throws Exception {
         Method fix = subnetterFragment.getClass().getDeclaredMethod("fix", String.class, String.class, int.class);
         fix.setAccessible(true);
-        assertEquals("003.000.000.001", fix.invoke(subnetterFragment, "003000000001", ".", 3));
+        for(int lp=0; lp<iterationsPerTest; lp++) {
+            String randomString = "";
+            int length = new Random().nextInt(100) + 10;
+            for (int i = 0; i < length; i++) {
+                randomString += (char) ('A' + new Random().nextInt('z' - 'A'));
+            }
+            int splitter = new Random().nextInt(8)+1;
+            String regex = "(?<=\\G";
+            for (int i = 0; i < splitter; i++) {
+                regex += ".";
+            }
+            regex += ")";
+            String[] randomStrings = randomString.split(regex);
+            String expectedOutput = "";
+            for (int i = 0; i < randomStrings.length-1; i++) {
+                expectedOutput += randomStrings[i] + ".";
+            }
+            expectedOutput += randomStrings[randomStrings.length-1];
+            assertEquals(expectedOutput, fix.invoke(subnetterFragment, randomString, ".", splitter));
+        }
     }
 
     @Test
     public void convertToDecimal() throws Exception {
         Method convertToDecimal = subnetterFragment.getClass().getDeclaredMethod("convertToDecimal", String.class);
         convertToDecimal.setAccessible(true);
-        assertEquals("255.0.1.2", convertToDecimal.invoke(subnetterFragment, "11111111.00000000.00000001.00000010"));
+        for(int lp=0; lp<iterationsPerTest; lp++) {
+            String decIP = "";
+            String binIP = "";
+            for (int i = 0; i < new Random().nextInt(100) + 1; i++) {
+                int rand = new Random().nextInt(255) + 1;
+                decIP += rand + ".";
+                binIP += Integer.toBinaryString(rand) + ".";
+            }
+            decIP = decIP.substring(0, decIP.length() - 1);
+            binIP = binIP.substring(0, binIP.length() - 1);
+            assertEquals(decIP, convertToDecimal.invoke(subnetterFragment, binIP));
+        }
     }
 
     @Test
     public void isValidCIDRChar() throws Exception {
         Method isValidCIDRChar = subnetterFragment.getClass().getDeclaredMethod("isValidCIDRChar", char.class);
         isValidCIDRChar.setAccessible(true);
-        assertTrue((boolean) isValidCIDRChar.invoke(subnetterFragment, '/'));
-        assertTrue((boolean) isValidCIDRChar.invoke(subnetterFragment, '5'));
-        assertFalse((boolean) isValidCIDRChar.invoke(subnetterFragment, 'a'));
+        for (int lp = 0; lp < iterationsPerTest; lp++) {
+            assertTrue((boolean) isValidCIDRChar.invoke(subnetterFragment, '/'));
+            assertTrue((boolean) isValidCIDRChar.invoke(subnetterFragment, (char) (new Random().nextInt('9'-'0')+'0')));
+            assertFalse((boolean) isValidCIDRChar.invoke(subnetterFragment, (char) (new Random().nextInt('z'-'a')+'a')));
+            assertFalse((boolean) isValidCIDRChar.invoke(subnetterFragment, (char) (new Random().nextInt('Z'-'A')+'A')));
+        }
     }
 }
